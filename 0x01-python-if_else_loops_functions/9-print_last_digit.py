604708710a5e12a3e49b2b90e375c84e7bae5b19
ossama11 <jeuxossama67@gmail.com>
2023-11-28 14:28:53 +0100

    task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,5 +2,5 @@
 def print_last_digit(number):
     strnumber = str(number)
     last = strnumber[-1]
-    print("{}".format(int(last)), end = "")
+    print("{}".format(int(last)), end="")
     return last
