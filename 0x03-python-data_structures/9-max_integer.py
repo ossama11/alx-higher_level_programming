5292c500661d9a85432548ba49c456c6a4198572
ossama11 <jeuxossama67@gmail.com>
2023-12-03 19:55:10 +0100

    task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return (None)
+    max_is = my_list[0]
+    for i in range(1, len(my_list)):
+        if my_list[i] > max_is:
+            max_is = my_list[i]
+    return (max_is)
